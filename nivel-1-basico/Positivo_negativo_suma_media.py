75a7aa442a95a02d9db12e868b985895e8633c59
Monibr3 <167772349+Monibr3@users.noreply.github.com>
2025-07-24 12:39:20 +0200

    Ejercicio:contar y sumar positivos y negativos

---
+++ nivel-1-basico/Positivo_negativo_suma_media.py
@@ -0,0 +1,25 @@
+#Programa que pide números al usuario hasta que introduzca un cero
+#Muestra por pantalla la cantidad de números positivos y negativos introducidos y sus respectivas sumas
+n=float(input("Introduce un número: "))
+neg = 0
+pos = 0
+suma_neg = 0
+suma_pos = 0
+while n != 0:
+    if n < 0:
+        neg += 1
+        suma_neg+=n
+    else:
+        pos += 1
+        suma_pos +=n
+    n=float(input("Introduce un número(0 para salir): "))
+print(f'Positivos {pos}\nNegativos {neg}')
+if pos > 0:
+    print(f"Media positivos = {suma_pos / pos}")
+else:
+    print("No se introdujeron números positivos")
+
+if neg > 0:
+    print(f"Media negativos = {suma_neg / neg}")
+else:
+    print("No se introdujeron números negativos")
